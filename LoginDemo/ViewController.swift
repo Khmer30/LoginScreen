5a68f2d6ba0e18f683a5c0dbd444c77758e2b3b5
marie <dul.sreyneang@gmail.com>
2023-02-11 14:01:27 +0800

    Fix UI

--- LoginDemo/ViewController.swift
+++ LoginDemo/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        self.emailTextField.addBottomBorderLineWithColor(color: .darkGray, width: 1.0)
+//        self.passwordTextField.addBottomBorderLineWithColor(color: .darkGray, width: 1.0)
+    
+//
+//        let bottomLine = CALayer()
+//
+//        bottomLine.frame = CGRect(x: 0, y: textField.frame.height - 2, width: textFiled.frame.width, height: 2)
+//
+//        bottomLine.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1).cgColor)
+//
+//        textField.BorderStyle = .none
+//
+//        textField.layer.addSublayer(layer: CALayer)
     }
-
-
+    
+/*OR*/
+    
+//    func addBottomBorderLineWithColor(color: UIColor, width: CGFloat) {
+//
+//        let bottomBorderLine = CALayer()
+//        bottomBorderLine.backgroundColor = color.cgColor
+//        bottomBorderLine.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: width)
+//        self.layer.addSublayer(bottomBorderLine)
 }
 
